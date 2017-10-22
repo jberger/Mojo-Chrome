ebf1a25d98d2956e54df6823a44d37d07cb909f8
Joel Berger <joel.a.berger@gmail.com>
2017-10-22 14:40:31 -0500

    add test library

---
+++ lib/Test/Mojo/Role/Chrome.pm
@@ -0,0 +1,57 @@
+package Test::Mojo::Role::Chrome;
+
+use Role::Tiny;
+use Mojo::Base -strict;
+use Mojo::Chrome;
+use Mojo::Util;
+use Test2::API ();
+
+__PACKAGE__->Mojo::Base::attr(chrome => sub { Mojo::Chrome->new });
+__PACKAGE__->Mojo::Base::attr('chrome_result');
+
+sub chrome_load_ok {
+  my ($self, $navigate, $desc) = @_;
+  $desc ||= 'Chrome navigate to page';
+  my $ctx = Test2::API::context();
+
+  my $ok = 0;
+  my $err;
+  $self->chrome->load_page($navigate, sub {
+    (undef, $err) = @_;
+    $ok = 1 unless $err;
+    Mojo::IOLoop->next_tick(sub{ Mojo::IOLoop->stop });
+  });
+  Mojo::IOLoop->start;
+
+  $ctx->diag($err) if $err;
+  $ctx->ok($ok, $desc);
+  $ctx->release;
+  return $self->success($ok);
+}
+
+sub chrome_evaluate_ok {
+  my ($self, $js, $desc) = @_;
+  $desc ||= 'Chrome evaluate';
+  my $ctx = Test2::API::context();
+
+  my $ok = 0;
+  my ($err, $result);
+  $self->chrome->evaluate($js, sub {
+    (undef, $err, $result) = @_;
+    $ok = 1 unless $err;
+    Mojo::IOLoop->next_tick(sub{ Mojo::IOLoop->stop });
+  });
+  Mojo::IOLoop->start;
+
+  $ctx->diag(Mojo::Util::dumper $err) if $err;
+  $ctx->ok($ok, $desc);
+  $ctx->release;
+  return $self->chrome_result($result)->success($ok);
+}
+
+sub chrome_result_is {
+
+}
+
+1;
+
---
+++ t/tester.t
@@ -0,0 +1,33 @@
+use Mojolicious::Lite;
+
+any '/' => 'main';
+
+use Test::More;
+use Test::Mojo;
+
+my $t = Test::Mojo->with_roles('+Chrome')->new;
+my $url = $t->ua->server->nb_url;
+
+$t->chrome_load_ok($url)
+  ->chrome_evaluate_ok(q!document.getElementsByTagName('p')[0].innerHTML!);
+
+is $t->chrome_result, 'Goodbye', 'correct updated result';
+
+done_testing;
+
+__DATA__
+
+@@ main.html.ep
+
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <p>Hello</p>
+    %= javascript begin
+      (function(){ document.getElementsByTagName('p')[0].innerHTML = 'Goodbye'; })();
+    % end
+  </body>
+</html>
+
+
