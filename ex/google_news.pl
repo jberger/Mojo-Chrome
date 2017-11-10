121d6274cc82f2ce1cc91e582141b1cf78c31df2
Joel Berger <joel.a.berger@gmail.com>
2017-11-09 20:23:28 -0600

    use the example for the main module synopsis
    also update the example to be more concise
    fixes #1

--- ex/google_news.pl
+++ ex/google_news.pl
@@ -2,21 +2,19 @@ use Mojo::Base -strict;
 
 use Mojo::Chrome;
 use Mojo::IOLoop;
-use Mojo::JSON;
 
 binmode(STDOUT, ":utf8");
 $|++;
 
-# this is the example from https://medium.com/@lagenar/using-headless-chrome-via-the-websockets-interface-5f498fb67e0f
-# archived at https://web.archive.org/web/20171020022803/https://medium.com/@lagenar/using-headless-chrome-via-the-websockets-interface-5f498fb67e0f
-# requires a running chome exposing debugging interface on port 9000
-# on my mac that is /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --remote-debugging-port=9000
+# This is the example from https://medium.com/@lagenar/using-headless-chrome-via-the-websockets-interface-5f498fb67e0f
+# of fetching the news headline from Google News. It should not be used as anything but an example.
+# It is archived at https://web.archive.org/web/20171020022803/https://medium.com/@lagenar/using-headless-chrome-via-the-websockets-interface-5f498fb67e0f
 
-my $chrome = Mojo::Chrome->new->catch(sub{ warn $_[1] });
+my $chrome = Mojo::Chrome->new->catch(sub{ pop });
 my $url = 'https://news.google.com/news/?ned=us&hl=en';
 
 Mojo::IOLoop->delay(
-  sub { $chrome->load_page({url => $url}, shift->begin) },
+  sub { $chrome->load_page($url, shift->begin) },
   sub {
     my ($delay, $err) = @_;
     die $err if $err;
--- lib/Mojo/Chrome.pm
+++ lib/Mojo/Chrome.pm
@@ -252,6 +252,41 @@ sub DESTROY {
 
 Mojo-Chrome - A Mojo interface to Chrome DevTools Protocol
 
+=head1 SYNOPSIS
+
+  # This is the example from https://medium.com/@lagenar/using-headless-chrome-via-the-websockets-interface-5f498fb67e0f
+  # of fetching the news headline from Google News. It should not be used as anything but an example.
+  # It is archived at https://web.archive.org/web/20171020022803/https://medium.com/@lagenar/using-headless-chrome-via-the-websockets-interface-5f498fb67e0f
+
+  use Mojo::Base -strict;
+
+  use Mojo::Chrome;
+  use Mojo::IOLoop;
+
+  binmode(STDOUT, ":utf8");
+  $|++;
+
+  my $chrome = Mojo::Chrome->new->catch(sub{ warn pop });
+  my $url = 'https://news.google.com/news/?ned=us&hl=en';
+
+  Mojo::IOLoop->delay(
+    sub { $chrome->load_page($url, shift->begin) },
+    sub {
+      my ($delay, $err) = @_;
+      die $err if $err;
+      $chrome->evaluate(<<'    JS', $delay->begin);
+        var sel = '[role="heading"][aria-level="2"]';
+        var headings = document.querySelectorAll(sel);
+        [].slice.call(headings).map((link)=>{return link.innerText});
+      JS
+    },
+    sub {
+      my ($delay, $err, $result) = @_;
+      die Mojo::Util::dumper $err if $err;
+      say for @$result;
+    }
+  )->catch(sub{ warn pop })->wait;
+
 =head1 DESCRIPTION
 
 L<Mojo::Chrome> is an interface to the Chrome DevTools Protocol which allows interaction with a (possibly headless) chrome instance.
